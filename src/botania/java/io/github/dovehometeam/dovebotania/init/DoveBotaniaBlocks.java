2bacf70c221f6905f7f0d0ce334d2aef57817d51
baka4n <qq474899581@gmail.com>
2025-09-19 15:58:52 +0800

    模型修改替换

--- src/botania/java/io/github/dovehometeam/dovebotania/init/DoveBotaniaBlocks.java
+++ src/botania/java/io/github/dovehometeam/dovebotania/init/DoveBotaniaBlocks.java
@@ -5,6 +5,7 @@ import com.tterrag.registrate.util.entry.BlockEntry;
 import io.github.dovehometeam.dovebotania.Const;
 import io.github.dovehometeam.dovebotania.common.block.BotaniaPetalApothecaryBlock;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.client.model.generators.BlockModelBuilder;
 import vazkii.botania.common.block.BotaniaBlocks;
 
@@ -18,38 +19,35 @@ import static io.github.dovehometeam.dovebotania.Dovebotania.REGISTRATE;
  * @code @Date 2025/9/5 14:18:20
  */
 public class DoveBotaniaBlocks {
-    public static final List<BlockEntry<BotaniaPetalApothecaryBlock>> APOTHECARIES =
-            petalDefaultRegistries(
-                    "apothecary_sandstone"
-            );
+    public static final List<BlockEntry<BotaniaPetalApothecaryBlock>> APOTHECARIES = new ArrayList<>();
+    public static final BlockEntry<BotaniaPetalApothecaryBlock> SANDSTONE = petalDefaultRegister(
+            "apothecary_sandstone",
+            ResourceLocation.withDefaultNamespace("sandstone"),
+            ResourceLocation.withDefaultNamespace("sandstone"),
+            ResourceLocation.withDefaultNamespace("sandstone")
+    );
 
-    public static List<BlockEntry<BotaniaPetalApothecaryBlock>> petalDefaultRegistries(String... names) {
-        ArrayList<BlockEntry<BotaniaPetalApothecaryBlock>> blockEntries = new ArrayList<>();
-        for (String name : names) {
-            blockEntries.add(petalDefaultRegister(name));
-        }
-        return blockEntries;
-    }
+    public static BlockEntry<BotaniaPetalApothecaryBlock> petalDefaultRegister(String name, ResourceLocation side, ResourceLocation bottom, ResourceLocation top) {
 
-    public static BlockEntry<BotaniaPetalApothecaryBlock> petalDefaultRegister(String name) {
-
-        return REGISTRATE
+        BlockEntry<BotaniaPetalApothecaryBlock> register = REGISTRATE
                 .block(name, BotaniaPetalApothecaryBlock::new)
                 .initialProperties(() -> BotaniaBlocks.defaultAltar)
                 .lang(RegistrateLangProvider.toEnglishName(name))
                 .blockstate((ctx, prov) -> {
                     BlockModelBuilder model = prov
                             .models()
-                            .withExistingParent(ctx.getName(), Const.modLoc("block/botania_petal_apothecary"))
-                            .texture("side", Const.modLoc("block/" + name + "_side"))
-                            .texture("bottom",  Const.modLoc("block/" + name + "_bottom"))
-                            .texture("top",  Const.modLoc("block/" + name + "_top"))
+                            .withExistingParent(ctx.getName(), Const.modLoc("block/petal_apothecary_default"))
+                            .texture("side", side.withPrefix("block/"))
+                            .texture("bottom", bottom.withPrefix("block/"))
+                            .texture("top", top.withPrefix("block/"))
                             .renderType(RenderType.cutout().name);
                     prov.simpleBlock(ctx.get(), model);
                 })
                 .item()
                 .build()
                 .register();
+        APOTHECARIES.add(register);
+        return register;
 
     }
 
