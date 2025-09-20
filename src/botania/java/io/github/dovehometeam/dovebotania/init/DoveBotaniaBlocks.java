ed08e41e4c4acda2407e54a750ca0be665d88ac2
baka4n <qq474899581@gmail.com>
2025-09-20 14:42:49 +0800

    添加更多花药台

--- src/botania/java/io/github/dovehometeam/dovebotania/init/DoveBotaniaBlocks.java
+++ src/botania/java/io/github/dovehometeam/dovebotania/init/DoveBotaniaBlocks.java
@@ -1,21 +1,18 @@
 package io.github.dovehometeam.dovebotania.init;
 
-import com.tterrag.registrate.providers.DataGenContext;
 import com.tterrag.registrate.providers.RegistrateLangProvider;
-import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import com.tterrag.registrate.util.DataIngredient;
 import com.tterrag.registrate.util.entry.BlockEntry;
-import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
 import io.github.dovehometeam.dovebotania.Const;
 import io.github.dovehometeam.dovebotania.common.block.BotaniaPetalApothecaryBlock;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.client.model.generators.BlockModelBuilder;
+import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import vazkii.botania.common.block.BotaniaBlocks;
 import vazkii.botania.common.lib.BotaniaTags;
 
@@ -36,7 +33,7 @@ public class DoveBotaniaBlocks {
             ResourceLocation.withDefaultNamespace("sandstone"),
             ResourceLocation.withDefaultNamespace("sandstone_bottom"),
             ResourceLocation.withDefaultNamespace("sandstone_top"),
-            () -> DataIngredient.items(Items.SANDSTONE)
+            () -> DataIngredient.tag(Tags.Items.SANDSTONE_BLOCKS)
     );
     public static final BlockEntry<BotaniaPetalApothecaryBlock> IRON = petalDefaultRegister(
             "apothecary_iron",
@@ -45,6 +42,21 @@ public class DoveBotaniaBlocks {
             ResourceLocation.withDefaultNamespace("iron_block"),
             () -> DataIngredient.items(Items.IRON_BLOCK)
     );
+    public static final BlockEntry<BotaniaPetalApothecaryBlock> GOLD = petalDefaultRegister(
+            "apothecary_gold",
+            ResourceLocation.withDefaultNamespace("gold_block"),
+            ResourceLocation.withDefaultNamespace("gold_block"),
+            ResourceLocation.withDefaultNamespace("gold_block"),
+            () -> DataIngredient.items(Items.GOLD_BLOCK)
+    );
+    public static final BlockEntry<BotaniaPetalApothecaryBlock> DIAMOND = petalDefaultRegister(
+            "apothecary_diamond",
+            ResourceLocation.withDefaultNamespace("diamond_block"),
+            ResourceLocation.withDefaultNamespace("diamond_block"),
+            ResourceLocation.withDefaultNamespace("diamond_block"),
+            () -> DataIngredient.items(Items.DIAMOND)
+    );
+
 
     @SafeVarargs
     public static BlockEntry<BotaniaPetalApothecaryBlock> petalDefaultRegister(String name,
@@ -70,6 +82,7 @@ public class DoveBotaniaBlocks {
                 .item()
                 .recipe((ctx, prov) -> {
                     DataIngredient petals = DataIngredient.tag(BotaniaTags.Items.PETALS);
+                    int i = 0;
                     for (var s : ss) {
                         DataIngredient ing = s.get();
                         ShapedRecipeBuilder
@@ -79,8 +92,9 @@ public class DoveBotaniaBlocks {
                                 .pattern("CCC")
                                 .define('P', petals.toVanilla())
                                 .define('C', ing.toVanilla())
-                                .unlockedBy("has_" + ctx.getName() + ing.getId(), ing.getCriterion(prov))
-                                .save(prov);
+                                .unlockedBy("has_" + ctx.getName() + "_" + ing.getId(), ing.getCriterion(prov))
+                                .save(prov, ctx.getId().withSuffix("_" + i));
+                        i++;
                     }
                 })
                 .build()
